af69c6f2425b86aa014edf0fae8d3f50ebc8df24
Miika Zitting <a1704460@myy.haaga-helia.fi>
2018-11-15 13:16:15 +0200

    Add setup sh

---
+++ startup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Copyright 2018 Miika Zitting http://miikazitting.wordpress.com GPL 3
+#Script to setup of a liveusb and set it up as a slave for master
+
+echo "Prepare yourself for the epic journey of salting your pc!"
+set -o verbose
+
+echo "Setting up your kblayout and installing salt-minion"
+
+setxkbmap fi
+sudo apt-get update
+sudo apt-get -y install salt-minion
+
+echo "Time to set you as a slave!"
+echo -e 'master: 104.248.162.226\nid: practisepc'|sudo tee /etc/salt/minion
+sudo systemctl restart salt-minion
